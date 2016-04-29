64f477fdba5f48b55a4becb5de21cc381b2992f5
saku <saku3ds@gmail.com>
2016-04-29 23:31:27 +0800

    modify data type
    When use String type,user to enter could have some problems.
    Modify data type (double).

--- Accurate.java
+++ Accurate.java
@@ -6,52 +6,52 @@ import java.math.BigDecimal;
 public class Accurate {
 
 	// Define data fields
-	private String x = 0;
-	private String y = 0;
+	private double x = 0;
+	private double y = 0;
 
 	// No-argument constructor
 	public Accurate(){
 	}
 
 	// Constructorto store two big integer
-	public Accurate(String newX,String newY){
+	public Accurate(double newX,double newY){
 		setX(newX);
 		setY(newY);
 	}
 
 	// Setting methods
-	public void setX(String newX){
+	public void setX(double newX){
 		x = newX;
 	}
-	public void setY(String newY){
+	public void setY(double newY){
 		y = newY;
 	}
 
 	// add method
 	public double add() {
-		BigDecimal num1 = new BigDecimal("x");
-		BigDecimal num2 = new BigDecimal("y");
+		BigDecimal num1 = new BigDecimal(Double.toString(x));
+		BigDecimal num2 = new BigDecimal(Double.toString(y));
 		return num1.add(num2).doubleValue();
 	}
 
 	// subtract method
 	public double sub(){
-		BigDecimal num1 = new BigDecimal("x");
-		BigDecimal num2 = new BigDecimal("y");
+		BigDecimal num1 = new BigDecimal(Double.toString(x));
+		BigDecimal num2 = new BigDecimal(Double.toString(y));
 		return num1.subtract(num2).doubleValue();
 	}
 
 	// multiply method
 	public double multi(){
-		BigDecimal num1 = new BigDecimal("x");
-		BigDecimal num2 = new BigDecimal("y");
+		BigDecimal num1 = new BigDecimal(Double.toString(x));
+		BigDecimal num2 = new BigDecimal(Double.toString(y));
 		return num1.multiply(num2).doubleValue();
 	}
 
 	// sub method
-	public double subtracInt(){
-			BigDecimal num1 = new BigDecimal("x");
-			BigDecimal num2 = new BigDecimal("y");
+	public double subtrac(){
+			BigDecimal num1 = new BigDecimal(Double.toString(x));
+			BigDecimal num2 = new BigDecimal(Double.toString(y));
 			return num1.divide(num2).doubleValue();
 	}
 }
